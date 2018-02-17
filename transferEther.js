e18b8ce1b82128e6e8fc7397e8f6904a0e1748f4
Kevin Wu <kevin.wu@ucsb.edu>
2018-02-17 13:50:21 -0800

    add transferEther

---
+++ transferEther.js
@@ -0,0 +1,59 @@
+//simple ether transfer script from one account to another using privateKey
+
+const {
+  web3,
+  provider,
+
+  toHex,
+  toWei,
+
+  loadContract,
+  solc,
+
+  EthereumTx
+} = require('./helpers')
+
+global.web3 = web3
+global.provider = provider
+global.toHex = toHex
+global.toWei = toWei
+global.loadContract = loadContract
+global.solc = solc
+global.EthereumTx = EthereumTx
+
+// ethereumjs-tx
+global.privateKey = Buffer.from(
+  '{private-key-goes-here}',
+  'hex'
+)
+
+const recipient = '{recipient-address-goes-here}'
+
+global.ethTx = {
+  //nonce is the 'transaction sequence'
+  nonce: '0x00',
+
+  gasPrice: web3.utils.toHex(web3.utils.toWei('20', 'gwei')),
+  gasLimit: web3.utils.toHex('200000'),
+
+  to: recipient,
+  value: web3.utils.toHex(web3.utils.toWei('1', 'ether')),
+  
+  // EIP 155 chainId - mainnet: 1, ropsten: 3
+  chainId: 3
+}
+
+//create transaction
+const tx = new EthereumTx(ethTx)
+
+//sign transaction
+tx.sign(privateKey)
+
+//send tx to chain
+web3.eth
+  .sendSignedTransaction(`0x${tx.serialize().toString('hex')}`)
+  // could take some time to be mined
+  .on('receipt', console.log)
+
+// Start repl
+require('repl').start({})
