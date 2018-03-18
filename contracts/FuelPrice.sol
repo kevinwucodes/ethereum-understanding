e620f1e508ee760f8bbdae9a2dbe9a095ad0fe73
Kevin Wu <kevin.wu@ucsb.edu>
2018-03-17 20:24:03 -0700

    add deployOracleDiesel

---
+++ contracts/FuelPrice.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.0;
+/* import "github.com/oraclize/ethereum-api/oraclizeAPI.sol"; */
+import "oraclize0.4.sol";
+
+contract FuelPrice is usingOraclize {
+
+    uint public DieselPriceUSD;
+
+    event newOraclizeQuery(string description);
+    event newDieselPrice(string price);
+
+    // function DieselPrice() {
+        // update(); // first check at contract creation
+    // }
+
+    function __callback(bytes32 myid, string result) {
+        if (msg.sender != oraclize_cbAddress()) throw;
+        newDieselPrice(result);
+        DieselPriceUSD = parseInt(result, 2); // let's save it as $ cents
+        // do something with the USD Diesel price
+    }
+
+    function update() payable {
+        newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+        oraclize_query("URL", "xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
+    }
+
+}
---
+++ ropsten/deployOracleDiesel.js
@@ -0,0 +1,90 @@
+const {
+  web3,
+  provider,
+
+  toHex,
+  toWei,
+
+  loadContract,
+  solc,
+
+  EthereumTx
+} = require('../helpers')
+
+global.web3 = web3
+global.provider = provider
+global.toHex = toHex
+global.toWei = toWei
+global.loadContract = loadContract
+global.solc = solc
+global.EthereumTx = EthereumTx
+
+//////////////////////////////////////
+
+global.rawSources = {
+  'oraclize0.4.sol': loadContract('./contracts/oraclize0.4.sol'),
+  'FuelPrice.sol': loadContract('./contracts/FuelPrice.sol')
+}
+
+global.compiled = solc.compile({ sources: rawSources })
+
+global.abi = JSON.parse(compiled.contracts['FuelPrice.sol:FuelPrice'].interface)
+global.bytecode = compiled.contracts['FuelPrice.sol:FuelPrice'].bytecode
+
+///////////////////////
+// ethereumjs-tx
+global.privateKey = Buffer.from(
+  '{your-private-key-here}',
+  'hex'
+)
+
+global.publicKey = web3.eth.accounts.privateKeyToAccount(
+  `0x${privateKey.toString('hex')}`
+).address
+
+global.ethTx = {
+  //nonce is the 'transaction sequence'
+  nonce: '0x0c',
+  gasPrice: web3.utils.toHex(web3.utils.toWei('15', 'gwei')), //0x037e11d600
+  gasLimit: web3.utils.toHex('2000000'), //0x1e8480
+
+  // give this contract some ether to power it
+  // value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
+
+  data: `0x${bytecode}`,
+
+  // EIP 155 chainId - mainnet: 1, ropsten: 3
+  chainId: 3
+}
+
+////////////////////////////
+//deploy the contract to ropsten
+////////////////////////////
+
+//create transaction
+global.tx = new EthereumTx(ethTx)
+
+//sign transaction
+// tx.sign(privateKey)
+//
+// //send tx to chain
+// web3.eth
+// .sendSignedTransaction(`0x${tx.serialize().toString('hex')}`)
+//   // could take some time to be mined
+// .on('receipt', console.log)
+
+////////////////////
+//after we deploy, lets interact with it
+
+// global.deployedContract = new web3.eth.Contract(
+//   abi,
+//   '0xf6dB51548698877421671b7bb15Aa8F58f0cFEeb'
+// )
+
+// deployedContract.methods
+//   .DieselPriceUSD()
+//   .call()
+//   .then(console.log)
+
+// Start repl
+require('repl').start({})
