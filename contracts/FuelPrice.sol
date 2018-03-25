bc248a4dc182e4d6f69fd4f5a959c0469b78c6bf
Kevin Wu <kevin.wu@ucsb.edu>
2018-03-25 10:50:21 -0700

    update FuelPrice with 2 oracles

--- contracts/FuelPrice.sol
+++ contracts/FuelPrice.sol
@@ -5,8 +5,15 @@ import "oraclize0.4.sol";
 contract FuelPrice is usingOraclize {
 
     uint public DieselPriceUSD;
+    string public currentTimeUTC;
 
     string private DIESEL_URL = "xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel";
+    string private UTCTIME_URL = "json(http://worldclockapi.com/api/json/utc/now).currentFileTime";
+
+    enum oracleTypes { diesel, time }
+
+    mapping(bytes32=>bool) public validOraclizeIds; //should be private IRL
+    mapping(bytes32=>oracleTypes) public oraclizeDescriptions; //should be private IRL
 
     event newOraclizeQuery(string description);
     event newDieselPrice(string price);
@@ -16,15 +23,40 @@ contract FuelPrice is usingOraclize {
     // }
 
     function __callback(bytes32 myid, string result) {
-        if (msg.sender != oraclize_cbAddress()) throw;
+      //if we dont have any valid oraclize IDs, get out of there
+      if (!validOraclizeIds[myid]) throw;
+
+      //if the caller isn't from the actual oracle service, get out of here as well
+      if (msg.sender != oraclize_cbAddress()) throw;
+
+      //was diesel called?
+      if (oraclizeDescriptions[myid] == oracleTypes.diesel) {
         newDieselPrice(result);
         DieselPriceUSD = parseInt(result, 2); // let's save it as $ cents
-        // do something with the USD Diesel price
+      }
+
+      //was time called?
+      if (oraclizeDescriptions[myid] == oracleTypes.time) {
+        currentTimeUTC = result;
+      }
+
+      //cleanup
+      delete validOraclizeIds[myid];
+      delete oraclizeDescriptions[myid];
     }
 
     function update() payable {
-        newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
-        oraclize_query("URL", DIESEL_URL);
+      newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+      bytes32 dieselId = oraclize_query("URL", DIESEL_URL);
+      validOraclizeIds[dieselId] = true;
+      oraclizeDescriptions[dieselId] = oracleTypes.diesel;
+    }
+
+    function updateTime() payable {
+      newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+      bytes32 timeId = oraclize_query("URL", UTCTIME_URL);
+      validOraclizeIds[timeId] = true;
+      oraclizeDescriptions[timeId] = oracleTypes.time;
     }
 
 }
--- ropsten/deployOracleDiesel.js
+++ ropsten/deployOracleDiesel.js
@@ -42,49 +42,91 @@ global.publicKey = web3.eth.accounts.privateKeyToAccount(
   `0x${privateKey.toString('hex')}`
 ).address
 
-global.ethTx = {
-  //nonce is the 'transaction sequence'
-  nonce: '0x11',
-  gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
-  gasLimit: web3.utils.toHex('2000000'), //0x1e8480
-
-  // give this contract some ether to power it
-  // value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
-
-  data: `0x${bytecode}`,
-
-  // EIP 155 chainId - mainnet: 1, ropsten: 3
-  chainId: 3
-}
-
 ////////////////////////////
 //deploy the contract to ropsten
 ////////////////////////////
 
-//create transaction
-global.tx = new EthereumTx(ethTx)
-
-//sign transaction
-tx.sign(privateKey)
+// global.ethTx = {
+//   //nonce is the 'transaction sequence'
+//   nonce: '0x14',
+//   gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+//   gasLimit: web3.utils.toHex('2000000'), //0x1e8480
 //
-// //send tx to chain
-web3.eth
-  .sendSignedTransaction(`0x${tx.serialize().toString('hex')}`)
-  //   // could take some time to be mined
-  .on('receipt', console.log)
+//   // give this contract some ether to power it
+//   // value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
+//
+//   data: `0x${bytecode}`,
+//
+//   // EIP 155 chainId - mainnet: 1, ropsten: 3
+//   chainId: 3
+// }
+
+// create transaction
+// global.tx = new EthereumTx(ethTx)
+//
+// //sign transaction
+// tx.sign(privateKey)
+// //
+// // //send tx to chain
+// web3.eth
+//   .sendSignedTransaction(`0x${tx.serialize().toString('hex')}`)
+//   //   // could take some time to be mined
+//   .on('receipt', console.log)
 
 ////////////////////
 //after we deploy, lets interact with it
 
-// global.deployedContract = new web3.eth.Contract(
-//   abi,
-//   '0xf6dB51548698877421671b7bb15Aa8F58f0cFEeb'
-// )
+global.deployedContract = new web3.eth.Contract(
+  abi,
+  '0x52AEd052c4004eF9bAa02236C7624B111B5Fcd5f'
+)
 
-// deployedContract.methods
-//   .DieselPriceUSD()
-//   .call()
-//   .then(console.log)
+//simple calls without ether costs
+deployedContract.methods
+  .DieselPriceUSD()
+  .call()
+  .then(value => console.log('diesel price is: ', value))
+
+deployedContract.methods
+  .currentTimeUTC()
+  .call()
+  .then(value => console.log('current time is: ', value))
+
+///////////////////
+//trying to execute method directly using a manually create eth transaction
+///////////////////
+// global.commandToCall = 'updateTime()'
+// global.commandToCallSha = web3.utils.sha3(commandToCall).slice(2, 10)
+//
+// global.ethTx = {
+//   //nonce is the 'transaction sequence'
+//   nonce: '0x19',
+//   gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+//   gasLimit: web3.utils.toHex('2000000'), //0x1e8480
+//
+//   // give this contract some ether to power it
+//   // value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
+//
+//   //contract address where fuelprice is deployed
+//   to: '0x52AEd052c4004eF9bAa02236C7624B111B5Fcd5f',
+//
+//   data: `0x${commandToCallSha}`,
+//
+//   // EIP 155 chainId - mainnet: 1, ropsten: 3
+//   chainId: 3
+// }
+//
+// // create transaction
+// global.tx = new EthereumTx(ethTx)
+//
+// //sign transaction
+// tx.sign(privateKey)
+// //
+// // //send tx to chain
+// web3.eth
+//   .sendSignedTransaction(`0x${tx.serialize().toString('hex')}`)
+//   //   // could take some time to be mined
+//   .on('receipt', console.log)
 
 // Start repl
 require('repl').start({})
